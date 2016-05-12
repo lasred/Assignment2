87273b14f7658b0b4b6e6c79a73e01e2ff3eaf2b
Chris Sun <chris438@gmail.com>
2016-05-12 13:11:32 -0700

    Fix PNG file extension bug

--- assetloader.js
+++ assetloader.js
@@ -7,8 +7,8 @@ AM.queueDownload("./img/MAGABackground.png");
 AM.queueDownload("./img/flags/spritesheet.png");
 AM.queueDownload("./img/CrookedHillary.png");
 AM.queueDownload("./img/LyinTed.png");
-AM.queueDownload("./img/Canada.png");
-AM.queueDownload("./img/ivanka.png");
+AM.queueDownload("./img/Canada.PNG");
+AM.queueDownload("./img/ivanka.PNG");
 AM.queueDownload("./img/reporter.png");
 AM.queueDownload("./img/cartel.png");
 AM.queueDownload("./img/assassin.png");
@@ -31,8 +31,8 @@ AM.downloadAll(function () {
     gameEngine.start();
     gameEngine.addEntity(new Background(gameEngine));
     gameEngine.addEntity(new TrumpWalker(gameEngine, AM.getAsset("./img/Trump.png")));
-    gameEngine.addEntity(new IvankaWalker(gameEngine, AM.getAsset("./img/ivanka.png"), 32, 50, 3, 100, 200, 0.1, 4, 100));
-	gameEngine.addEntity(new Bullet(gameEngine, AM.getAsset("./img/Canada.png")))
+    gameEngine.addEntity(new IvankaWalker(gameEngine, AM.getAsset("./img/ivanka.PNG"), 32, 50, 3, 100, 200, 0.1, 4, 100));
+	gameEngine.addEntity(new Bullet(gameEngine, AM.getAsset("./img/Canada.PNG")))
 	gameEngine.addEntity(new ReporterWalker(gameEngine, AM.getAsset("./img/reporter.png"), 175, 150, 11, 500, 100, 0.5, 100, 120));
 	gameEngine.addEntity(new CartelWalker(gameEngine, AM.getAsset("./img/cartel.png"), 75, 190, 4, 500, 400, 0.5, 100, 80));
 	gameEngine.addEntity(new AssassinWalker(gameEngine, AM.getAsset("./img/assassin.png"), 33, 48, 4, 100,  400, 0.1, 4, 100));
